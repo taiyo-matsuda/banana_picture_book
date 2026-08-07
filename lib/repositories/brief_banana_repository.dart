fa64215bd8a59fb86970537104350842599daffd
taiyo-matsuda <235474578+taiyo-matsuda@users.noreply.github.com>
2026-08-07 23:08:12 +0900

    update brief banana repository

--- lib/repositories/brief_banana_repository.dart
+++ lib/repositories/brief_banana_repository.dart
@@ -1,5 +1,5 @@
 import 'package:banana_picture_book/models/brief_banana.dart';
 
 abstract class BriefBananaRepository {
-  Future<List<BriefBanana>> getBriefBananas();
+  Future<List<BriefBanana>> findAll();
 }
--- lib/repositories/brief_banana_repository_imple.dart
+++ lib/repositories/brief_banana_repository_imple.dart
@@ -1,12 +1,27 @@
+import 'package:banana_picture_book/mappers/brief_banana_mapper.dart';
 import 'package:banana_picture_book/models/brief_banana.dart';
 import 'package:banana_picture_book/repositories/brief_banana_repository.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class BriefBananaRepositoryImpl implements BriefBananaRepository {
-  @override
-  Future<List<BriefBanana>> getBriefBananas() async {
-    // TODO:
-    // 後でSupabaseから取得する
+  final SupabaseClient supabase;
 
-    return [];
+  BriefBananaRepositoryImpl(this.supabase);
+
+  @override
+  Future<List<BriefBanana>> findAll() async {
+    final response = await supabase.from('varieties').select('''
+          *,
+          accessions(
+            *,
+            species(*),
+            subspecies(*)
+          ),
+          origins(*)
+        ''');
+
+    return response
+        .map<BriefBanana>((data) => BriefBananaMapper.fromMap(data))
+        .toList();
   }
 }
